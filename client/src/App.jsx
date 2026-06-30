42825641b63ee86b2fd05644dc5bc608ace2262d
Anee006 <aneesha.sibyj@gmail.com>
2026-06-30 16:30:29 +0530

    Modify form to enable Add Student function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,43 +12,33 @@ function App() {
     const fetchStudents = async () => {
 
         try {
-
             const response = await API.get("/students");
 
             setStudents(response.data);
-
         }
 
         catch(err){
-
             console.log(err);
-
         }
-
     };
 
     useEffect(() => {
-
         fetchStudents();
-
     }, []);
 
     return (
-
         <div className="container">
 
             <h1>Student Record Management System</h1>
 
-            <StudentForm />
+            <StudentForm fetchStudents={fetchStudents} />
 
             <SearchBar />
 
             <StudentList students={students}/>
 
         </div>
-
     );
-
 }
 
 export default App;
--- client/src/components/StudentForm.jsx
+++ client/src/components/StudentForm.jsx
@@ -1,16 +1,95 @@
-function StudentForm() {
+import { useState } from "react";
+import API from "../services/api";
+
+function StudentForm({ fetchStudents }) {
+
+    const [formData, setFormData] = useState({
+        name: "",
+        age: "",
+        department: "",
+        semester: "",
+        email: ""
+    });
+
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+
+            [e.target.name]: e.target.value
+        });
+    };
+
+    const handleSubmit = async (e) => {
+
+        e.preventDefault();
+
+        try {
+            await API.post("/students", formData);
+
+            fetchStudents();
+
+            setFormData({
+                name: "",
+                age: "",
+                department: "",
+                semester: "",
+                email: ""
+            });
+        }
+
+        catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <div>
             <h2>Add Student</h2>
 
-            <form>
-                <input type="text" placeholder="Name" />
-                <input type="number" placeholder="Age" />
-                <input type="text" placeholder="Department" />
-                <input type="number" placeholder="Semester" />
-                <input type="email" placeholder="Email" />
+            <form onSubmit={handleSubmit}>
 
-                <button>Add Student</button>
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    value={formData.name}
+                    onChange={handleChange}
+                />
+
+                <input
+                    type="number"
+                    name="age"
+                    placeholder="Age"
+                    value={formData.age}
+                    onChange={handleChange}
+                />
+
+                <input
+                    type="text"
+                    name="department"
+                    placeholder="Department"
+                    value={formData.department}
+                    onChange={handleChange}
+                />
+
+                <input
+                    type="number"
+                    name="semester"
+                    placeholder="Semester"
+                    value={formData.semester}
+                    onChange={handleChange}
+                />
+
+                <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    value={formData.email}
+                    onChange={handleChange}
+                />
+
+                <button type="submit">Add Student</button>
+
             </form>
         </div>
     );
